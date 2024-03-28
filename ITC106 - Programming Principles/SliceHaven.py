1eb2b3ad7667d45bb3b6b523a2a993dfc5b67d21
SpeedyPete25 <mitch.johnston25@gmail.com>
2024-03-28 22:59:36 +1030

    Seperated printed comments from order validation

--- ITC106 - Programming Principles/SliceHaven.py
+++ ITC106 - Programming Principles/SliceHaven.py
@@ -11,6 +11,9 @@ def order_pizza():
     #Defines the price for pizza sizes Personal/Medium/Family
     price = [8, 12, 16]
 
+    #Check to validate correct order entry
+    valid_order = True
+
     #Prints instructions and input options for user
     print("Welcome to Slice Haven!")
     print("Here are the available options:")
@@ -21,36 +24,52 @@ def order_pizza():
     print("Personal")
     print("Medium")
     print("Family")
+    
+    #presents user with input for order
     pizza_type = input("Enter the type of pizza you want: ")
     size = input("Enter the size of pizza (Personal/Medium/Family): ")
 
-    #Prints start of order
-    print("\nYour order summary:")
-    print(f"Type: {pizza_type}, Size: {size}")
+    #Assigns the pizza type order ratios
+    if pizza_type == "Margherita":
+        order = margherita
+    elif pizza_type == "Vegetarian":
+        order = vegetarian
+    elif pizza_type == "Meat Lovers":
+        order = meat
+    else:
+        #Returns error comment if incorrect type entered
+        print("Invalid Pizza Type Entered")
+        #prevents for loop running if order invalid
+        valid_order = False
 
-    #Prints each ingredient ratio for selected pizza type
-    print("Ingredient Ratios:")
-    for i in range(3):
-        if pizza_type == "Margherita":
-            print(f"\t{ratios[i]}: {margherita[i]}%")
-        elif pizza_type == "Vegetarian":
-            print(f"\t{ratios[i]}: {vegetarian[i]}%")
-        elif pizza_type == "Meat Lovers":
-            print(f"\t{ratios[i]}: {meat[i]}%")
-        else:
-            #Returns error comment if incorrect type entered
-            print("Invalid Pizza Type Entered")
-
-    #Prints the cost of the pizza based on size selected
+    #Finds the price of the ordered pizza size
     if size == "Personal":
-        print(f"Total Cost: ${price[0]}")
+        order_price = price[0]
     elif size == "Medium":
-        print(f"Total Cost: ${price[1]}")
+        order_price = price[1]
     elif size == "Family":
-        print(f"Total Cost: ${price[2]}")
+        order_price = price[2]
     else:
         #Returns error comment if incorrect size entered
         print("Invalid Pizza Size Entered")
+        #prevents for loop running if order invalid
+        valid_order = False
+
+    #Check if pizza type is valid
+    if valid_order == True:
+        #Prints start of order
+        print("\nYour order summary:")
+        print(f"Type: {pizza_type}, Size: {size}")
+
+        #Prints pizza ingredient ratios for the order
+        print("Ingredient Ratios:")
+        for i in range(3):
+            print(f"\t{ratios[i]}: {order[i]}%")
+
+        #Prints the price of the pizza order
+        print(f"Total Cost: ${order_price}")
+
+        
 
 #Runs the function to order pizza
 order_pizza()
